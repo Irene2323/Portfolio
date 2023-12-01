d9d8836de31f63d3326e5794b657e2c1cbe8b548
Irene2323 <141957731+Irene2323@users.noreply.github.com>
2023-12-01 14:34:35 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -13,53 +13,53 @@ console.log(typeof age);
 
 
     
-        document.getElementById("btn-alert").addEventListener("click", function() {
-            alert("Hello world!");
-        });
+document.getElementById("btn-alert").addEventListener("click", function() {
+    alert("Hello world!");
+});
     
 
     
-        document.getElementById('hoverBtn').onmouseover = () => {
-            document.getElementById('hoverBtn').innerText = 'Click this Button';
-        };
+document.getElementById('hoverBtn').onmouseover = () => {
+    document.getElementById('hoverBtn').innerText = 'Click this Button';
+    };
         
-        document.getElementById('hoverBtn').onmouseleave = () => {
-            document.getElementById('hoverBtn').innerText = 'default button label';
-        };
+document.getElementById('hoverBtn').onmouseleave = () => {
+    document.getElementById('hoverBtn').innerText = 'default button label';
+};
    
 
 
 
     
-        // incrementing button
-        let count = 1;
+// incrementing button
+let count = 1;
 
-        document.getElementById('btn-counter').onclick = () => {
-            count = count + 1;
-            document.getElementById('txt-counter').innerHTML = `Number: ${count}`;
+document.getElementById('btn-counter').onclick = () => {
+    count = count + 1;
+    document.getElementById('txt-counter').innerHTML = `Number: ${count}`;
         
     
-            //  if-else statement
-            if (count % 2 === 0) {
-                document.getElementById('txt-counter').classList.add('even');
-                document.getElementById('txt-counter').classList.remove('odd');
-            } else {
-                document.getElementById('txt-counter').classList.add('odd');
-                document.getElementById('txt-counter').classList.remove('even');
-            }
-        }
+//  if-else statement
+    if (count % 2 === 0) {
+        document.getElementById('txt-counter').classList.add('even');
+        document.getElementById('txt-counter').classList.remove('odd');
+    } else {
+        document.getElementById('txt-counter').classList.add('odd');
+        document.getElementById('txt-counter').classList.remove('even');
+    }
+}
    
 
     
 
    
-        //  list element
-        var numbersList = document.getElementById('numbers');
+//  list element
+var numbersList = document.getElementById('numbers');
 
-        //  for loop 
-        for (let i = 1; i <= 100; i++) {
-            var listItem = document.createElement('li');
-            listItem.textContent = i % 2 === 0 ? 'even' : 'odd';
-            numbersList.appendChild(listItem);
-        }
+//  for loop 
+for (let i = 1; i <= 100; i++) {
+    var listItem = document.createElement('li');
+    listItem.textContent = i % 2 === 0 ? 'even' : 'odd';
+    numbersList.appendChild(listItem);
+}
     
